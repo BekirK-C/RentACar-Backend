7c6bcffa05e613396a08f6fd449c3bd24cf5e57f
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-10-08 22:14:46 +0300

    Entity Framework

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
@@ -8,7 +10,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarManager carManager = new CarManager(new EfCarDal());
 
             foreach (var car in carManager.GetAll())
             {
