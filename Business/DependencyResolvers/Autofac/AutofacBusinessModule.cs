8084d65056a7ac3af8056a9f33a4684a9253e71f
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-10-17 17:00:10 +0300

    Images Services

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -3,6 +3,7 @@ using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
 using Castle.DynamicProxy;
+using Core.Utilities.Helpers.FileHelper;
 using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
@@ -34,6 +35,11 @@ namespace Business.DependencyResolvers.Autofac
 
             builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
             builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
+
+            builder.RegisterType<CarImageManager>().As<ICarImageService>().SingleInstance();
+            builder.RegisterType<EfCarImageDal>().As<ICarImageDal>().SingleInstance();
+
+            builder.RegisterType<FileHelperManager>().As<IFileHelper>().SingleInstance();
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
--- Core/Utilities/Helpers/FileHelper/FileHelperManager.cs
+++ Core/Utilities/Helpers/FileHelper/FileHelperManager.cs
@@ -9,45 +9,45 @@ using System.Threading.Tasks;
 
 namespace Core.Utilities.Helpers.FileHelper
 {
-    // Projeme yükleyeceğim dosyalarla ilgili yükleme,silme,güncelleme işlemlerini bu class!ımda yapıyorum.
-    //İşlemin nasıl gerçekleştiğini anlamak için yazdığım yorum satırlarını okumaya En alttaki *Upload* metodunundan başlayabilirsiniz.
-    public class FileHeplerManager : IFileHelper
+    // Projeye yüklenen dosyalarla ilgili işlemlerini (yükleme, silme, güncelleme) bu class'da yapıyoruz.
+    public class FileHelperManager : IFileHelper
     {
-        public void Delete(string filePath)//Burada ki string filePath, 'CarImageManager'dan gelen dosyamın kaydedildiği adres ve adı 
+        public void Delete(string filePath)  // Buradaki string filePath; "CarImageManager" dan gelen dosyanin kaydedildiği adres ve adı
         {
-            if (File.Exists(filePath))//if kontrolü ile parametrede gelen adreste öyle bir dosya var mı diye kontrol ediliyor.
+            if (File.Exists(filePath))  // Gelen adreste dosyanın olup olmadığını kontrol ediyoruz.
             {
-                File.Delete(filePath);//Eğer dosya var ise dosya bulunduğu yerden siliniyor.
+                File.Delete(filePath);  // Eğer dosya var ise siliniyor.
             }
         }
 
-        public string Update(IFormFile file, string filePath, string root)//Dosya güncellemek için ise gelen parametreye baktığımızda Güncellenecek yeni dosya, Eski dosyamızın kayıt dizini, ve yeni bir kayıt dizini
+        public string Update(IFormFile file, string filePath, string root)
         {
-            if (File.Exists(filePath))// Tekrar if kontrolü ile parametrede gelen adreste öyle bir dosya var mı diye kontrol ediliyor.
+            if (File.Exists(filePath))  // Gelen adreste dosyanın olup olmadığını kontrol ediyoruz.
             {
-                File.Delete(filePath);//Eğer dosya var ise dosya bulunduğu yerden siliniyor.
+                File.Delete(filePath);  // Eğer dosya var ise siliniyor.
             }
-            return Upload(file, root);// Eski dosya silindikten sonra yerine geçecek yeni dosyaiçin alttaki *Upload* metoduna yeni dosya ve kayıt edileceği adres parametre olarak döndürülüyor.
+            return Upload(file, root);  // Eski dosya silindikten sonra yerine geçecek yeni dosya için Upload metodu ile yeni dosyayı ekliyoruz.
         }
 
         public string Upload(IFormFile file, string root)
         {
-            if (file.Length > 0)//file.Length=>Dosya uzunluğunu bayt olarak alır. burada Dosya gönderil mi gönderilmemiş diye test işlemi yapıldı.
+            if (file.Length > 0)  // Dosyanın gönderildiği dosya uzunluğundan kontrol ediliyor.
             {
-                if (!Directory.Exists(root))//Directory=>System.IO'nın bir class'ı. burada ki işlem tam olarak şu. Bu Upload metodumun parametresi olan string root CarManager'dan gelmekte
-                {                           //CarImageManager içerisine girdiğinizde buraya parametre olarak *PathConstants.ImagesPath* böyle bir şey gönderilidğini görürsünüz. PathConstants clası içerisine girdiğinizde string bir ifadeyle bir dizin adresi var
-                                            //O adres bizim Yükleyeceğimiz dosyaların kayıt edileceği adres burada *Check if a directory Exists* ifadesi şunu belirtiyor dosyanın kaydedileceği adres dizini var mı? varsa if yapısının kod bloğundan ayrılır eğer yoksa içinde ki kodda dosyaların kayıt edilecek dizini oluşturur
+                if (!Directory.Exists(root))
+                {
+                    // Directory; System.IO'nun bir class'ıdır. Buradaki işlem tam olarak şu. Bu Upload metodumun parametresi olan string root CarManager'dan gelmekte
+                    // CarImageManager içerisine girdiğinizde buraya parametre olarak *PathConstants.ImagesPath* böyle bir şey gönderilidğini görürsünüz. PathConstants clası içerisine girdiğinizde string bir ifadeyle bir dizin adresi var. O adres bizim yükleyeceğimiz dosyaların kayıt edileceği adres burada *Check if a directory Exists* ifadesi şunu belirtiyor dosyanın kaydedileceği adres dizini var mı? varsa if yapısının kod bloğundan ayrılır eğer yoksa içinde ki kodda dosyaların kayıt edilecek dizini oluşturur.
                     Directory.CreateDirectory(root);
                 }
-                string extension = Path.GetExtension(file.FileName);//Path.GetExtension(file.FileName)=>> Seçmiş olduğumuz dosyanın uzantısını elde ediyoruz.
-                string guid = GuidHelper.CreateGuid();//Core.Utilities.Helpers.GuidHelper klasürünün içinde ki GuidManager klasörüne giderseniz burada satırda ne yaptığımızı anlayacaksınız
-                string filePath = guid + extension;//Dosyanın oluşturduğum adını ve uzantısını yan yana getiriyorum. Mesela metin dosyası ise .txt gibi bu projemizde resim yükyeceğimiz için .jpg olacak uzantılar 
+                string extension = Path.GetExtension(file.FileName);  // Seçmiş olduğumuz dosyanın uzantısını elde ediyoruz.
+                string guid = GuidHelper.CreateGuid();  // Core.Utilities.Helpers.GuidHelper klasöründeki GuidManager class'ında oluşturduğumuz eşsiz ismi alıyoruz.
+                string filePath = guid + extension;  // İsim ile uzantıyı birleştiriyoruz.
 
-                using (FileStream fileStream = File.Create(root + filePath))//Burada en başta FileStrem class'ının bir örneği oluşturulu., sonrasında File.Create(root + newPath)=>Belirtilen yolda bir dosya oluşturur veya üzerine yazar. (root + newPath)=>Oluşturulacak dosyanın yolu ve adı.
+                using (FileStream fileStream = File.Create(root + filePath))  // FileStream class'ının bir örneği oluşturuldu, sonrasında ise "root + filePath" yolunda bir dosya oluşturur.
                 {
-                    file.CopyTo(fileStream);//Kopyalanacak dosyanın kopyalanacağı akışı belirtti. yani yukarıda gelen IFromFile türündeki file dosyasınınnereye kopyalacağını söyledik.
-                    fileStream.Flush();//arabellekten siler.
-                    return filePath;//burada dosyamızın tam adını geri gönderiyoruz sebebide sql servere dosya eklenirken adı ile eklenmesi için.
+                    file.CopyTo(fileStream);  // Yukarıdan gelen IFormFile türündeki file dosyasını nereye kopyalayacağını belirttik.
+                    fileStream.Flush();  // Arabellekte silme.
+                    return filePath;  // Sql servere dosya eklenirken adı ile eklenmesi için dosyamızın tam adını geri döndürüyoruz.
                 }
             }
             return null;
